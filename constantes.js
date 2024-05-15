5fef6b3a1c1bc4dc6b5be068bf8f9d1c51d04478
gustavo <91570363+Itsizanagi@users.noreply.github.com>
2024-05-14 21:08:42 -0300

    Create constantes.js

---
+++ constantes.js
@@ -0,0 +1,16 @@
+// const nome = 'arquivo1';
+// console.log(nome);
+const teste = '5';
+const text = "arquivo de texto";
+const number1 = 10;
+const number2 = 10;
+const conta = number1 * number2;
+const conta2 = conta * 2;
+let conta3 = conta * 3;
+conta3 = conta3 + 5;
+
+console.log(teste + number1); // concatenação
+console.log(number1 + number2);
+console.log(typeof (number1));
+console.log(typeof (text));
+console.log(conta3);
